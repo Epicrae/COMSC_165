992edce1f55d86c536e6a7caaa736945e46f2e88
Zack Garza <dzackgarza@gmail.com>
2014-08-21 16:12:09 -0700

    Assignment 1 Added

---
+++ Assignment 1/Assignment1/main.cpp
@@ -0,0 +1,62 @@
+/*
+ * Author:      Zack Garza
+ * Assignment:  Assignment 1
+ * Date:        8/21/2014
+ * Description: A program that performs a survey tally on favorite types
+        of beverages. Each person selects a beverage from a list, and
+        entering -1 stops the program and performs the tally.
+ */
+
+#include <iostream>
+using namespace std;
+
+void function1()
+{
+    cout << "Beverage choices: "
+        << endl << "1: Coffee"
+        << endl << "2: Tea"
+        << endl << "3: Coke"
+        << endl << "4: Orange Juice"
+        << endl;
+
+    int numberOfPeople = 0, choice = 0;
+    int votes[4];
+
+    for (int i = 0; i < 4; i++) {
+        votes[i] = 0;
+    }
+
+    while (choice != -1) {
+
+        cout << "Please input the favorite beverage of person " << (numberOfPeople + 1) << ": ";
+        cin >> choice;
+
+        while (choice < -1 || choice > 4 || choice == 0) {
+            cout << "Invalid selection, please enter a value from 1-4, or -1 to exit.";
+            cin >> choice;
+        }
+
+        votes[choice-1]++;
+        numberOfPeople++;
+    }
+
+    cout << "Beverage choices: "
+        << endl << "1: Coffee: "          << votes[0]
+        << endl << "2: Tea: "             << votes[1]
+        << endl << "3: Coke: "            << votes[2]
+        << endl << "4: Orange Juice: "    << votes[3]
+        << endl;
+
+}
+
+
+int main ()
+{
+    function1();
+    return 0;
+}
+
+
+
+
+
