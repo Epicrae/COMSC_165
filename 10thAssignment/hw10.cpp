599cfacc3491a2eb09649cc80a496e6d38c672ee
Zack Garza <dzackgarza@gmail.com>
2014-11-26 02:02:04 -0800

    Cleanup +comments

--- 10thAssignment/hw10.cpp
+++ 10thAssignment/hw10.cpp
@@ -3,7 +3,7 @@
 #include<assert.h>
 #include<climits>
 
-#define debug false
+#define debug false 
 
 using namespace std;
 
@@ -60,6 +60,10 @@ void printEntry(const entry*, int);
 // to guarantee its invariant sortedness.
 void sortList(linkedList&);
 
+// Run assertion tests.
+void runTests();
+/*---------------------------------------------------------------*/
+
 
 /* -----------------------Search Methods:-------------------- */
 
@@ -69,88 +73,93 @@ bool search(const linkedList&, int searchNumber);
 // Get the actual entry. Returns a nullptr if searchNumber is not found.
 entry* searchNumber(const linkedList&, int searchNumber, bool internal);
 entry* searchNumber(const linkedList&, int searchNumber);
+/*---------------------------------------------------------------*/
+
+
+/*------------------------Helper Functions--------------------*/
+
+// Valid menu choices
+enum MENU_CHOICE { ADD, DELETE, DISPLAY, QUIT};
+
+// How many entries are in MENU_CHOICE
+int num_options = 4;
+
+// How many times to allow incorrect input before exiting program
+int menu_error_chances = 5;
+
+// Convert int input into an entry in the MENU_CHOICE enum
+MENU_CHOICE getMenuChoice();
+
+// Route choice to program functionality
+void processMenuChoice(const MENU_CHOICE, linkedList&);
+
+// Prompts the user (up to menu_error_chances times) for an integer in 
+// the range (min, max]
+int getValidatedInt(const int min, const int max);
+
+// Pretty-print menu options to stdout
+void print_menu_options();
+/*---------------------------------------------------------------*/
+
+
+
 
 /* -----------------------Implementation:-------------------- */
+
+
+/* -----------------------Publicly Accessible Methods -------*/
 
 // Creates a list and performs several tests.
 int main(void) {
 
+	if(debug) runTests();
+
+	MENU_CHOICE choice;
 	linkedList* list = new linkedList;
+	do 
+	{
+		choice = getMenuChoice();
+		if(debug) cout << "Main: Menu choice was: " << choice+1 << endl;
+		processMenuChoice(choice, *list);
 
-	addNumber(*list, 5);
-	assert (list->contents[0].number == 5); 
+	} while (choice != QUIT);
 
-	addNumber(*list, 4);
-	assert (list->contents[0].number == 4 && 
-			list->contents[1].number == 5);
-
-	addNumber(*list, 3);
-	assert (list->contents[0].number == 3 && 
-			list->contents[1].number == 4 &&
-			list->contents[2].number == 5);
-
-	addNumber(*list, 2);
-	assert (list->contents[0].number == 2 && 
-			list->contents[1].number == 3 &&
-			list->contents[2].number == 4 &&
-			list->contents[3].number == 5);
-
-	addNumber(*list, 1);
-	assert (list->contents[0].number == 1 && 
-			list->contents[1].number == 2 &&
-			list->contents[2].number == 3 &&
-			list->contents[3].number == 4 &&
-			list->contents[4].number == 5);
-
-	cout << "------- \"Add Number\" tests passed." << endl;
-
-	displayList(*list);
-
-	assert (search(*list, 1) == true);
-	assert (search(*list, 2) == true);
-	assert (search(*list, 3) == true);
-	assert (search(*list, 4) == true);
-	assert (search(*list, 5) == true);
-	
-	assert (search(*list, INT_MIN) == false);
-	assert (search(*list, INT_MAX) == false);
-
-	cout << "------- \"Search\" tests passed." << endl;
-
-	assert (deleteNumber(*list, 6) == false);
-	assert (deleteNumber(*list, 0) == false);
-	assert (deleteNumber(*list, INT_MIN) == false);
-	assert (deleteNumber(*list, INT_MAX) == false);
-
-	assert (deleteNumber(*list, 3) == true);
-	assert (list->contents[0].number == 1 && 
-			list->contents[1].number == 2 &&
-			list->contents[2].number == 4 &&
-			list->contents[3].number == 5);
-
-	assert(deleteNumber(*list, 1) == true);
-	assert (list->contents[0].number == 2 && 
-			list->contents[1].number == 4 &&
-			list->contents[2].number == 5);
-
-	assert(deleteNumber(*list, 5) == true);
-	assert (list->contents[0].number == 2 && 
-			list->contents[1].number == 4); 
-
-	assert(deleteNumber(*list, 2) == true);
-	assert (list->contents[0].number == 4); 
-
-	assert(deleteNumber(*list, 2) == false);
-
-	assert(deleteNumber(*list, 4) == true);
-	assert(list->contents.size() == 0); 
-
-	cout << "------- \"deleteNumber\" tests passed." << endl;
 	return 0;
 
 }
 
+void addNumber(linkedList& list, int number) {
+
+	cout << "Adding " << number << " to the linked list." << endl;
+
+	// Create a new entry holding the number
+	entry* e = new entry;
+	e->number = number;
+	e->nextElement = nullptr;
+
+	// Add it into the list, letting vector take care of memory allocation
+	list.contents.push_back(*e);
+
+	// If this is the first item going into the list, initialize it
+	if (list.head == nullptr) {
+		cout << "List was empty. Initializing with new entry." << endl;
+		list.head = e;
+
+	// Otherwise, traverse the list, and add this entry at the appropriate point
+	} else {
+		entry* current = list.head;
+		while (current->nextElement != nullptr)
+			current = current->nextElement;	
+		current->nextElement = e;
+	}
+
+	// Ensure the list is always in a sorted state to maintain invariant.
+	sortList(list);
+}
+
 bool deleteNumber(linkedList& list, int num) {
+
+	if(debug) cout << "Called deleteNumber." << endl;
 
 	// Get the first entry where 'num' occurs
 	entry* e = searchNumber(list, num);
@@ -189,32 +198,7 @@ bool deleteNumber(linkedList& list, int num) {
 	return false;
 }
 
-// Note: terribly inefficient! Insertion sort may work better, but
-// this is a very simple implementation for now
-void sortList(linkedList& list) {
 
-	// First, sort the underlying array
-	// Just a basic bubble sort 
-	for (int i = 0; i < list.contents.size()-1; i++) {
-		for (int j = 0; j < list.contents.size()-1; j++) {
-			if (list.contents[j].number > list.contents[j+1].number) {
-				entry temp = list.contents[j];
-				list.contents[j] = list.contents[j+1];
-				list.contents[j+1] = temp;
-			}
-		}
-	}
-
-	// Fix the 'head' pointer, and unlink the last node's 'next' pointer
-	list.head = &list.contents.front();
-	list.contents.back().nextElement = nullptr;
-
-	// Now, go through and fix the rest of the 'next' pointers.
-	for (int i =0; i < list.contents.size() - 1; i++) {
-		list.contents[i].nextElement = &list.contents[i + 1];
-	}
-
-}
 void displayList(const linkedList& list) {
 
 	// Use 1-based indexing for human-readable display
@@ -240,29 +224,20 @@ void displayList(const linkedList& list) {
 	}
 }
 
-// Internal method, only for pretty-printing entries
-void printEntry(const entry* entry, int index) {
-
-	cout << "----------------------" << endl;
-	cout << "Entry " << index << ":" << endl;
-	cout << "Number: " << entry->number << endl;
-	if (entry->nextElement != nullptr)
-		cout << "Next: " << entry->nextElement->number << endl;
-	cout << "----------------------" << endl;
-}
-
+// Use if you just want to know /if/ the number was found
 bool search(const linkedList& list, int num) {
-
 	if (debug) cout << "Boolean search function called." << endl;
-
 	return (searchNumber(list, num, true) == nullptr ? false : true);
 }
-		
 
+// Use if you need to actually work with the entry if it is found
 entry* searchNumber(const linkedList& list, int number) {
+	if(debug) cout << "Called searchNumber from an external method." << endl;
 	return searchNumber(list, number, false);
 }
 
+// Set internal=true to suppress stdout output. Useful for testing.
+// For external calls, just use the two-arg searchNumber
 entry* searchNumber(const linkedList& list, int number, bool internal) {
 
 	if(debug) cout << "Entry search function called." << endl;
@@ -276,7 +251,10 @@ entry* searchNumber(const linkedList& list, int number, bool internal) {
 	// Keep track of whether or not number has been found yet
 	bool found = false;
 
-	if (debug) if(!current) cout << "Current pointer is null." << endl;
+	if(!current) {
+		if (debug) cout << "Current pointer is null." << endl;
+		return nullptr;
+	}
 
 	// If the search term is less than the first number, it can't be in the list
 	if (number < current->number) {
@@ -315,31 +293,224 @@ entry* searchNumber(const linkedList& list, int number, bool internal) {
 	return current;
 }
 
-void addNumber(linkedList& list, int number) {
 
-	cout << "Adding " << number << " to the linked list." << endl;
+/* ---------------------------- Internal Methods ------------------------*/
 
-	// Create a new entry holding the number
-	entry* e = new entry;
-	e->number = number;
-	e->nextElement = nullptr;
+// Note: terribly inefficient! Insertion sort may work better, but
+// this is a very simple implementation for now
+void sortList(linkedList& list) {
 
-	// Add it into the list, letting vector take care of memory allocation
-	list.contents.push_back(*e);
-
-	// If this is the first item going into the list, initialize it
-	if (list.head == nullptr) {
-		cout << "List was empty. Initializing with new entry." << endl;
-		list.head = e;
-
-	// Otherwise, traverse the list, and add this entry at the appropriate point
-	} else {
-		entry* current = list.head;
-		while (current->nextElement != nullptr)
-			current = current->nextElement;	
-		current->nextElement = e;
+	// First, sort the underlying array
+	// Just a basic bubble sort 
+	for (int i = 0; i < list.contents.size()-1; i++) {
+		for (int j = 0; j < list.contents.size()-1; j++) {
+			if (list.contents[j].number > list.contents[j+1].number) {
+				entry temp = list.contents[j];
+				list.contents[j] = list.contents[j+1];
+				list.contents[j+1] = temp;
+			}
+		}
 	}
 
-	// Ensure the list is always in a sorted state to maintain invariant.
-	sortList(list);
+	// Fix the 'head' pointer, and unlink the last node's 'next' pointer
+	list.head = &list.contents.front();
+	list.contents.back().nextElement = nullptr;
+
+	// Now, go through and fix the rest of the 'next' pointers.
+	for (int i =0; i < list.contents.size() - 1; i++) {
+		list.contents[i].nextElement = &list.contents[i + 1];
+	}
+
+}
+// Internal method, only for pretty-printing entries
+void printEntry(const entry* entry, int index) {
+
+	cout << "----------------------" << endl;
+	cout << "Entry " << index << ":" << endl;
+	cout << "Number: " << entry->number << endl;
+	if (entry->nextElement != nullptr)
+		cout << "Next: " << entry->nextElement->number << endl;
+	cout << "----------------------" << endl;
+}
+
+void processMenuChoice(const MENU_CHOICE choice, linkedList& list) {
+	
+	switch(choice) {
+		case ADD:
+		{
+			cout << "Enter number to be added:";
+			int new_int = getValidatedInt(INT_MIN, INT_MAX);
+			addNumber(list, new_int);
+			cout << new_int << " added." << endl;
+			break;
+		}
+		case DELETE:
+		{
+			cout << "Enter number to be deleted:";
+			int del_int = getValidatedInt(INT_MIN, INT_MAX);
+			bool deleted = deleteNumber(list, del_int);
+			if (deleted) cout << del_int << " deleted." << endl;
+			else cout << "Number not found in list." << endl;
+			break;
+		}
+		case DISPLAY:
+		{
+			displayList(list);
+			break;
+		}
+		case QUIT:
+		{
+			cout << "Thank you for using the best linked-list "
+				<< "implementation of all time!" << endl;
+			exit(0);
+		}
+		default:
+		{
+			cout << "Error: invalid menu choice received. Please try again." << endl;
+			break;
+		}
+	}
+
+}
+
+void print_menu_options() {
+	cout 
+		<< "1: Add a Number To List" << endl
+		<< "2: Delete a Number From List" << endl 
+		<< "3: Display List" << endl
+		<< "4: Exit Program" << endl
+		<< "Enter menu choice: ";
+}
+
+MENU_CHOICE getMenuChoice() {
+
+	int chances = menu_error_chances;
+	print_menu_options();
+	int choice = getValidatedInt(0, num_options);	
+
+	while (!(1 <= choice && choice <= num_options)) {
+
+		if (--chances < 0) {
+			cout << "Input error. exiting program." << endl;
+			exit(1);
+		}
+
+		cout << "Invalid choice, please enter again.";
+		print_menu_options();
+		cin.clear(); cin.ignore(INT_MAX,'\n'); 
+		choice = getValidatedInt(0, num_options);
+	}
+
+
+	if(debug) cout << "getMenuChoice: Switching menu choice:"<<choice<<endl;
+
+	switch (choice) {
+		case 1: return ADD;
+		case 2: return DELETE;
+		case 3: return DISPLAY;
+		case 4: return QUIT;
+		default:
+			cout << "Fatal error." << endl;
+			exit(-1);
+	}
+}
+
+/* Ensures input integer is in the range (min, max]. */
+int getValidatedInt(const int min, const int max) {
+
+	if(debug) cout << "Getting integer in range (" << min <<","<<max<<"]"<<endl;
+	int chances = menu_error_chances;
+	int temp;
+	cin >> temp;
+
+	while (!(temp > min && temp <= max)) {
+		if (--chances < 0) { 
+			cout << "Error: too many invalid inputs received. Exiting.." << endl;
+			exit(1); 
+		}
+		cout << "Number out of range, please try again:" ;
+		cin >> temp;
+	}
+	if(debug) cout << "Got int:"<<temp<<endl;
+	
+	// Clear input buffer so next input doesn't pick up extra chars
+	cin.clear(); cin.ignore(INT_MAX,'\n');
+	cout << endl;
+	return temp;
+}
+
+void runTests() {
+	linkedList* list = new linkedList;
+
+	addNumber(*list, 5);
+	assert (list->contents[0].number == 5); 
+
+	addNumber(*list, 4);
+	assert (list->contents[0].number == 4 && 
+			list->contents[1].number == 5);
+
+	addNumber(*list, 3);
+	assert (list->contents[0].number == 3 && 
+			list->contents[1].number == 4 &&
+			list->contents[2].number == 5);
+
+	addNumber(*list, 2);
+	assert (list->contents[0].number == 2 && 
+			list->contents[1].number == 3 &&
+			list->contents[2].number == 4 &&
+			list->contents[3].number == 5);
+
+	addNumber(*list, 1);
+	assert (list->contents[0].number == 1 && 
+			list->contents[1].number == 2 &&
+			list->contents[2].number == 3 &&
+			list->contents[3].number == 4 &&
+			list->contents[4].number == 5);
+
+	if(debug) cout << "------- \"Add Number\" tests passed." << endl;
+
+	if (debug) displayList(*list);
+
+	assert (search(*list, 1) == true);
+	assert (search(*list, 2) == true);
+	assert (search(*list, 3) == true);
+	assert (search(*list, 4) == true);
+	assert (search(*list, 5) == true);
+
+	assert (search(*list, INT_MIN) == false);
+	assert (search(*list, INT_MAX) == false);
+
+	cout << "------- \"Search\" tests passed." << endl;
+
+	assert (deleteNumber(*list, 6) == false);
+	assert (deleteNumber(*list, 0) == false);
+	assert (deleteNumber(*list, INT_MIN) == false);
+	assert (deleteNumber(*list, INT_MAX) == false);
+
+	assert (deleteNumber(*list, 3) == true);
+	assert (list->contents[0].number == 1 && 
+			list->contents[1].number == 2 &&
+			list->contents[2].number == 4 &&
+			list->contents[3].number == 5);
+
+	assert(deleteNumber(*list, 1) == true);
+	assert (list->contents[0].number == 2 && 
+			list->contents[1].number == 4 &&
+			list->contents[2].number == 5);
+
+	assert(deleteNumber(*list, 5) == true);
+	assert (list->contents[0].number == 2 && 
+			list->contents[1].number == 4); 
+
+	assert(deleteNumber(*list, 2) == true);
+	assert (list->contents[0].number == 4); 
+
+	assert(deleteNumber(*list, 2) == false);
+
+	assert(deleteNumber(*list, 4) == true);
+	assert(list->contents.size() == 0); 
+
+	if (debug) cout << "------- \"deleteNumber\" tests passed." << endl;
+
+	delete list;
 }
